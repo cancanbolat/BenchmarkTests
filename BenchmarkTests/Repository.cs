ed0c7d2e13937a7e127f5aab3ef7bba904e4c630
cancanbolat <can.canbolat12@gmail.com>
2023-03-19 17:52:06 +0300

    update repository

--- BenchmarkTests/Repository.cs
+++ BenchmarkTests/Repository.cs
@@ -21,6 +21,13 @@ public class Repository<T> where T : class
             query = query.AsNoTracking();
         return await query.FirstOrDefaultAsync(expression);
     }
+    public T GetSingle(Expression<Func<T, bool>> expression, bool tracking = true)
+    {
+        var query = Table.AsQueryable();
+        if (!tracking)
+            query = query.AsNoTracking();
+        return query.FirstOrDefault(expression)!;
+    }
 
     public IQueryable<T> GetWhere(Expression<Func<T, bool>> expression, bool tracking = true)
     {
@@ -58,6 +65,21 @@ public class Repository<T> where T : class
 
         return await query.ToListAsync();
     }
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>>? predicate, IList<string>? includes = null, bool disableTracking = true)
+    {
+        IQueryable<T> query = _dbContext.Set<T>();
+
+        if (disableTracking) query = query.AsNoTracking();
+
+        if (includes != null)
+        {
+            query = includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (predicate != null) query = query.Where(predicate);
+
+        return query.ToList();
+    }
 
     public async Task<T> GetByPredicateAsync(Expression<Func<T, bool>>? predicate, IList<string>? includes = null, bool disableTracking = true)
     {
@@ -73,5 +95,20 @@ public class Repository<T> where T : class
         if (predicate != null) query = query.Where(predicate);
 
         return await query.FirstOrDefaultAsync();
+    }
+    public T GetByPredicate(Expression<Func<T, bool>>? predicate, IList<string>? includes = null, bool disableTracking = true)
+    {
+        IQueryable<T> query = _dbContext.Set<T>();
+
+        if (disableTracking) query = query.AsNoTracking();
+
+        if (includes != null)
+        {
+            query = includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (predicate != null) query = query.Where(predicate);
+
+        return query.FirstOrDefault()!;
     }
 }
